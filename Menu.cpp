e5c83501ceec31e17be8be232e65842c86549697
Jack London <Jack@thelondons.com>
2025-11-20 14:52:32 -0600

    Implement Menu class with display functionality

---
+++ Menu.cpp
@@ -0,0 +1,25 @@
+#include "Menu.h"
+#include <iostream>
+#include <cstdlib> // for system("cls")
+
+using namespace std;
+
+Menu::Menu(string title, list<string> options)
+{
+    this->title = title;
+    this->options = options;
+}
+
+Menu::~Menu() {}
+
+void Menu::Display()
+{
+    int optionCounter = 0;
+    system("cls");
+    cout << "--------------- " << this->title << " ---------------" << endl;
+    for (const string& option : this->options)
+    {
+        optionCounter += 1;
+        cout << optionCounter << ". " << option << endl;
+    }
+}
