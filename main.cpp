866e2422f7f5c9ce1fe2123d299065afe3d4fb1d
Jack London <Jack@thelondons.com>
2025-11-20 14:53:54 -0600

    Refactor main function and menu handling

--- main.cpp
+++ main.cpp
@@ -1,138 +1,47 @@
 #include <iostream>
-#include <string>
-#include <conio.h>
 #include <list>
+#include <windows.h>
+
+#include "Menu.h"
+#include "Account.h"
+
 using namespace std;
-
-class Menu
-{
-private:
-    string title;
-    list<string> options;
-public:
-    Menu(string title, list<string> options)
-    {
-        this->title = title;
-        this->options = options;
-    }
-    ~Menu() {}
-
-    void Display()
-    {
-        int optionCounter = 0;
-
-        cout << "--------------- " << this->title << " ---------------" << endl;
-        for (string option : this->options)
-        {
-            optionCounter += 1;
-
-            cout << optionCounter << ". " << option << endl;
-        }
-    }
-};
-
-string getPassword() {
-    std::string password;
-    char ch;
-
-    while (true) {
-        ch = _getch();  
-
-        if (ch == 13) { 
-            std::cout << std::endl;
-            break;
-        }
-        else if (ch == 8) {
-            if (!password.empty()) {
-                password.pop_back();
-                std::cout << "\b \b"; 
-            }
-        }
-        else {
-            password.push_back(ch);
-            std::cout << "*"; 
-        }
-    }
-
-    return password;
-}
-
-int CreateAccount()
-{
-    string fName, lName, username;
-
-    double balance = 0.0;
-
-    system("cls");
-
-
-    cout << "Please enter First Name: ";
-    cin >> fName;
-
-    cout << "Please enter Last Name: ";
-    cin >> lName;
-
-    cout << "Please enter a Username: ";
-    cin >> username;
-
-    cout << "Please create a Password: ";
-    string password =  getPassword();
-
-    cout << "Please confirm your new Password: ";
-    string confPassword = getPassword();
-
-    while (password != confPassword)
-    {
-        cout << "Your passwords do not match." << endl;
-
-        cout << "Please create a Password: ";
-        password = getPassword();
-
-        cout << "Please confirm your new Password: ";
-        confPassword = getPassword();
-    }
-
-
-    // TODO: Save Logic ------------------
-
-    system("cls");
-    cout << "Account created!\nWelcome to our bank " << fName << " " << lName << " (" << username << ")." << endl;
-    cout << "\n\nPress Enter to continue...";
-    cin.ignore();
-    cin.get();
-
-    return 0;
-}
 
 int main()
 {
-    list<string> mainMenuOptions = { "User Login", "Create Account","Manager Login", "Exit" };
-    Menu mainMenu = Menu("Main Menu", mainMenuOptions);
-    mainMenu.Display();
+    SetConsoleOutputCP(CP_UTF8);  // so ► prints correctly
 
+    list<string> mainMenuOptions = { "User Login", "Create Account", "Manager Login", "Exit" };
+    Menu mainMenu("Main Menu", mainMenuOptions);
 
-    int userInput;
-    cin >> userInput;
-    while (userInput != 4 && userInput <= 4)
+    int userInput = 0;
+
+    while (true)
     {
+        mainMenu.Display();
+
+        cout << u8"► ";
+        cin >> userInput;
+
         switch (userInput)
         {
         case 1:
-            //User Login page
+            // User Login page (TODO)
             break;
         case 2:
             CreateAccount();
             break;
         case 3:
-            //User Login page
+            // Manager Login page (TODO)
             break;
         case 4:
-            cout << "Have a nice day!" << endl;
+            quit();
+            return 0;
+        default:
+            // Invalid option
             break;
         }
-
-        mainMenu.Display();
-        cin >> userInput;
     }
 
+    return 0;
 }
