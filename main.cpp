16d66ba494d247e8157340c390b5dcb0c1b90f57
Ethan Christman <ethan.christman@live.com>
2025-12-01 21:35:59 -0600

    Unused
    Use Main.cpp not this file

--- main.cpp
+++ main.cpp
@@ -1,46 +1,47 @@
-#include <iostream>
-#include <list>
-#include <windows.h>
-#include "Menu.h"
-#include "Account.h"
+// #include <iostream>
+// #include <list>
+// #include <windows.h>
+// #include "Menu.h"
+// #include "Account.h"
 
-using namespace std;
+// using namespace std;
 
-int main()
-{
-    SetConsoleOutputCP(CP_UTF8);
+// int main()
+// {
+//     SetConsoleOutputCP(CP_UTF8);
 
-    list<string> mainMenuOptions = { "User Login", "Create Account", "Manager Login", "Exit" };
-    Menu mainMenu("Main Menu", mainMenuOptions);
+//     list<string> mainMenuOptions = { "User Login", "Create Account", "Manager Login", "Exit" };
+//     Menu mainMenu("Main Menu", mainMenuOptions);
 
-    int userInput = 0;
+//     int userInput = 0;
 
-    while (true)
-    {
-        mainMenu.Display();
+//     while (true)
+//     {
+//         mainMenu.Display();
 
-        cout << u8"► ";
-        cin >> userInput;
+//         cout << u8"► ";
+//         cin >> userInput;
 
-        switch (userInput)
-        {
-        case 1:
-            // User Login page (TODO)
-            break;
-        case 2:
-            CreateAccount();
-            break;
-        case 3:
-            // Manager Login page (TODO)
-            break;
-        case 4:
-            quit();
-            return 0;
-        default:
-            // Invalid option
-            break;
-        }
-    }
+//         switch (userInput)
+//         {
+//         case 1:
+//             // User Login page (TODO)
+//             break;
+//         case 2:
+//             CreateAccount();
+//             break;
+//         case 3:
+//             // Manager Login page (TODO)
+//             break;
+//         case 4:
+//             quit();
+//             return 0;
+//         default:
+//             // Invalid option
+//             break;
+//         }
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
